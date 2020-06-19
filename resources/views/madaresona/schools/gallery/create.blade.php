60e7abd474a0e3c1ef990a6deb36485a01a789d5
Omar <omarmaree97@gmail.com>
2020-06-19 17:38:44 +0300

    glary/icon/omar

--- resources/views/madaresona/schools/gallery/create.blade.php
+++ resources/views/madaresona/schools/gallery/create.blade.php
@@ -6,16 +6,22 @@
         box-shadow: 0 0.5rem 1.5rem 0.5rem rgba(0, 0, 0, .075);
         border: 3px solid #fff;
     }
+    .btn_close
+    {
+        position: absolute;
+        right: 50px;
+        top: -8px;
+    }
 </style>
 
 <div class="row">
 
     @foreach($gallery as $item)
-    <div class="col-md-3 galleryDeleteDiv_{{ $item->id }}">
-        <label class="btn btn-xs btn-icon btn-circle btn-white btn-hover-text-primary btn-shadow galleryDelete" id="{{ $item->id }}" style="color: #262673 !important;">
-            <i class="fa fa-pen icon-sm text-muted"></i>
+    <div class="col-md-3 galleryDeleteDiv_{{ $item->id }} form-group">
+        <label class="btn_close btn btn-xs btn-icon btn-circle btn-white btn-hover-text-primary btn-shadow galleryDelete" id="{{ $item->id }}" style="color: #262673 !important;">
+            <i class="fa fa-times text-muted" style="color: red !important;"></i>
         </label>
-        <img class="img-gallery" src="{{ asset('/images/'.$schoolName.'/gallery/'.$item->img) }}" width="100px" height="100px" alt="">
+        <img class="img-gallery" src="{{ asset('/images/'.$schoolName.'/gallery/'.$item->img) }}" width="200px" height="150px" alt="">
     </div>
         @endforeach
 </div>
@@ -26,8 +32,7 @@
         <input type="hidden" name="school_id" value="{{ $id }}">
         <div class="col-md-3">
             <div class="form-group">
-                <input id="galleries" name="galleries[]" type="file" class="file" data-show-upload="true"
-                       data-show-caption="true" multiple>
+                <input id="galleries" name="galleries[]" type="file" class="file" multiple>
             </div>
         </div>
         <input type="submit" class="btn btn-success" value="Submit" style="float: right">
