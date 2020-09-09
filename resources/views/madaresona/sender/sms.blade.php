59b3bc4e48b4f510a41a55a0d8a2bb9212988db1
OmarMarie <omarmaree97@gmail.com>
2020-09-09 16:52:30 +0300

    add

--- resources/views/madaresona/sender/sms.blade.php
+++ resources/views/madaresona/sender/sms.blade.php
@@ -15,15 +15,12 @@
                     <div class="row">
                         <div class="col-md-12 form-group">
                             <label>Number's</label>
-                            <input type="file" name="title"  class="form-control">
+                            <input type="file" name="title" class="form-control">
                         </div>
                         <div class="col-md-12 form-group">
                             <label style="display: block">Content</label>
                             <textarea name="sms_text" id="sms_text"
-                                      placeholder="Text ........................" style="margin-top: 0px;
-    margin-bottom: 0px;
-    height: 200px;
-    width: 800px;"></textarea>
+                                      placeholder="Text ........................" style="margin-top: 0px; margin-bottom: 0px; height: 200px;width: 800px;"></textarea>
                         </div>
                         <div class="col-md-12 form-group">
                             <input type="submit" class="btn btn-success" name="Submit" style="float: right">
